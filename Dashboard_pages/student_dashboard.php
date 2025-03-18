5c3fea7ba5198421312be2e63eeec90b562b6fe2
pmcguffin <patrick.mcguffin.22@cnu.edu>
2025-03-18 10:32:52 -0400

    Update student_dashboard.php

--- Dashboard_pages/student_dashboard.php
+++ Dashboard_pages/student_dashboard.php
@@ -34,7 +34,7 @@ if (!isset($_SESSION['Student_Email'])) {
             <!-- Buttons for different professor features -->
             <a href="account_settings.php" class="btn">Account Settings</a><br>
             <a href="messages.php" class="btn">Messages</a><br>
-            <a href="jobs.php" class="btn">Job Posts</a><br>
+            <a href="student_jobs_menu.php" class="btn">Job Posts</a><br>
             <!-- <a href="students.php" class="btn">Students</a><br><br> -->
 
             <a href="logout.php" class="btn logout-btn">Logout</a> <!-- Logout option -->
