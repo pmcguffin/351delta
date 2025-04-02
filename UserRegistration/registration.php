c074176163732fbf80ff79bfa6e9f3b232593d15
josh-jones22 <135622104+josh-jones22@users.noreply.github.com>
2025-04-02 10:45:24 -0400

    Update registration.php

--- UserRegistration/registration.php
+++ UserRegistration/registration.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "351delta2";
+$dbname = "351delta";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
